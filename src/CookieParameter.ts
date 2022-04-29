bf7194233936c8dc8c6a8a5a9bee97c805900384
Himenon <k.himeno314@gmail.com>
2022-04-29 19:49:37 +0900

    fix: Serialize QueryParameter with URLSearchParams

--- src/CookieParameter.ts
+++ src/CookieParameter.ts
@@ -1,5 +1,5 @@
-import { PrimitiveType, ObjectType, ArrayType, ParameterOfForm } from "./Types";
-import * as Guard from "./Guard";
+import { PrimitiveType, ObjectType, ArrayType } from "./Types";
+import * as Core from "./Core";
 
 export interface Parameter {
   value: PrimitiveType | ObjectType | ArrayType;
@@ -9,36 +9,7 @@ export interface Parameter {
 
 export const generate = (key: string | number, params: Parameter): string | undefined => {
   if (params.style === "form") {
-    return generateFormParamter(key, params);
+    return Core.generateFormParamter(key, params);
   }
   return undefined;
-};
-
-const generateFormParamter = (key: string | number, params: ParameterOfForm): string => {
-  if (Guard.isEmpty(params.value)) {
-    return `${key}=`;
-  }
-  if (Guard.isPrimitive(params.value)) {
-    return `${key}=${params.value}`;
-  }
-  if (Guard.isArray(params.value)) {
-    if (params.explode) {
-      return params.value.map(item => `${key}=${item}`).join(";");
-    } else {
-      return `${key}=${params.value.join(",")}`;
-    }
-  }
-  if (Guard.isObject(params.value)) {
-    if (params.explode) {
-      return Object.entries(params.value)
-        .map(([k, v]) => `${k}=${v}`)
-        .join(";");
-    } else {
-      const value = Object.entries(params.value)
-        .map(([k, v]) => `${k},${v}`)
-        .join(",");
-      return `${key}=${value}`;
-    }
-  }
-  return `${key}=`;
 };
